7b80fbca54a7407966ed2808c405d8fa741e4cc3
Lucas-Sloan <Lucasg.sloan@gmail.com>
2024-09-10 15:00:55 -0500

    Finished first half of exercises

---
+++ README.md
@@ -0,0 +1,54 @@
+
+```
+django-crud-app-cat-collector
+├─ Pipfile
+├─ Pipfile.lock
+├─ README.md
+├─ catcollector
+│  ├─ __init__.py
+│  ├─ asgi.py
+│  ├─ settings.py
+│  ├─ urls.py
+│  └─ wsgi.py
+├─ db.sqlite3
+├─ main_app
+│  ├─ __init__.py
+│  ├─ admin.py
+│  ├─ apps.py
+│  ├─ migrations
+│  │  └─ __init__.py
+│  ├─ models.py
+│  ├─ templates
+│  │  └─ main_app
+│  │     ├─ about.html
+│  │     ├─ base.html
+│  │     └─ cats
+│  │        └─ index.html
+│  ├─ tests.py
+│  ├─ urls.py
+│  └─ views.py
+├─ manage.py
+└─ static
+   ├─ css
+   │  ├─ base.css
+   │  └─ cats
+   │     └─ cat-index.css
+   └─ images
+      ├─ cat-cone.svg
+      ├─ cat-in-box.svg
+      ├─ cat-onigiri.svg
+      ├─ cool-cat.svg
+      ├─ fish.svg
+      ├─ happy-cat.svg
+      ├─ header-logo.svg
+      ├─ kitty-kabob.svg
+      ├─ logotype.svg
+      ├─ mouse.svg
+      ├─ nerd-cat.svg
+      ├─ post.svg
+      ├─ sk8r-boi-cat.svg
+      ├─ splash.svg
+      ├─ string.svg
+      └─ teacup-cat.svg
+
+```
---
+++ main_app/views.py
@@ -0,0 +1,33 @@
+# main_app/views.py
+
+from django.shortcuts import render
+
+from django.http import HttpResponse  # Import HttpResponse to return plain HTML content
+
+def home(request):
+    return render(request, 'main_app/base.html')
+
+def about(request):
+    return render(request, 'main_app/about.html')
+
+def home(request):
+    return render(request, 'main_app/home.html')
+
+class Cat:
+    def __init__(self, name, breed, description, age):
+        self.name = name
+        self.breed = breed
+        self.description = description
+        self.age = age
+
+# Create a list of Cat instances
+cats = [
+    Cat('Lolo', 'tabby', 'Kinda rude.', 3),
+    Cat('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
+    Cat('Fancy', 'bombay', 'Happy fluff ball.', 4),
+    Cat('Bonk', 'selkirk rex', 'Meows loudly.', 6)
+]
+
+def cat_index(request):
+    # Render the cats/index.html template with the cats data
+    return render(request, 'main_app/cats/index.html', {'cats': cats})
